56b51d2a9340b8913e0e97586e245a04d426896f
Therese Tessman <therese.tessman@gmail.com>
2018-10-03 22:47:30 -0700

    markers showing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    venues: []
+  }
 
   componentDidMount() {
-    this.loadMap()
+    this.getVenues()
   }
 
   loadMap = () => {
@@ -14,12 +17,41 @@ class App extends Component {
     window.initMap = this.initMap
   }
 
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?",
+      parameters = {
+        client_id: "",
+        client_secret: "",
+        query: "tacos",
+        near: "Sydney",
+        v: "20181003"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        }, this.loadMap())
+      })
+      .catch(error => {
+        console.log("ERROR!!" + error)
+      })
+  }
+
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
       zoom: 8
-    });
-  }
+    })
+
+    this.state.venues.map(myVenue => {
+
+    const marker = new window.google.maps.Marker({
+      position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
+      map: map
+    })
+  })
+}
 
   render() {
     return (
